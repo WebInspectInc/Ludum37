52a110e40b728bd8960bf656a25a4372f2cafcff
Timothy Miller <tjacobdesign@gmail.com>
2016-12-10 13:58:47 -0600

    Adds much better hitboxes

--- source/entities/Cockroach.hx
+++ source/entities/Cockroach.hx
@@ -23,6 +23,9 @@ class Cockroach extends Entity {
 		animation.add('walk', [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25], 25);
 		animation.play('walk');
 
+		setSize(130, 88);
+		offset.set(41, 86);
+
 		health = 20;
 
 		this.moveSpeed = 100;
