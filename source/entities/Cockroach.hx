5e8d3f5effc8275f00d1337b955d35164cf79eaf
Timothy Miller <tjacobdesign@gmail.com>
2016-12-10 13:22:02 -0600

    Adds cockroach sprites

--- source/entities/Cockroach.hx
+++ source/entities/Cockroach.hx
@@ -9,6 +9,8 @@ import flixel.group.FlxGroup;
 import entities.Entity;
 
 class Cockroach extends Entity {
+	private static inline var SPRITE_HEIGHT:Int = 256;
+	private static inline var SPRITE_WIDTH:Int = 252;
 
 	private var accelerationSpeed:Int = 1000;
 
@@ -16,7 +18,12 @@ class Cockroach extends Entity {
 
 	public function new(?X:Float=0, ?Y:Float=0, Group:FlxGroup) {
 		super(X, Y, Group);
-		sprite.makeGraphic(16, 16, FlxColor.ORANGE);
+		//sprite.makeGraphic(16, 16, FlxColor.ORANGE);
+		sprite.loadGraphic(AssetPaths.roach_walk__png, true, SPRITE_WIDTH, SPRITE_HEIGHT);
+
+		sprite.animation.add('walk', [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25], 25);
+		sprite.animation.play('walk');
+
 		health = 20;
 
 		this.moveSpeed = 100;
