a907e6d865295f6fb381fd10c87788c031fbc959
Timothy Miller <tjacobdesign@gmail.com>
2016-12-10 20:17:50 -0600

    Makes explosions hurt the player

--- source/entities/bullets/Explosion.hx
+++ source/entities/bullets/Explosion.hx
@@ -11,6 +11,7 @@ class Explosion extends Bullet {
 	private static inline var SPRITE_WIDTH:Int = 132;
 
 	private var explosionSound:FlxSound = FlxG.sound.load(AssetPaths.explosion_short__wav, .2);
+	private var hurtPlayer:Bool = false;
 
 	override public function new(X:Float, Y:Float, State:PlayState, Speed:Int, Direction:Float, Damage:Float, Group:FlxGroup) {
 		super(X, Y, State, Speed, Direction, Damage, Group);
@@ -18,12 +19,22 @@ class Explosion extends Bullet {
 
 		animation.add('explode', [0,1,2,3,4,5,6,7,8,9], 25, false);
 		animation.play('explode');
+
+		setSize(132, 120);
+		offset.set(0, -16);
 	}
 
 	override public function update(delta:Float) {
 		if (animation.finished) {
+			if (hurtPlayer) {
+				state.player.hurt(4);
+			}
 			kill();
 			return;
+		}
+
+		if (overlaps(state.player)) {
+			hurtPlayer = true;
 		}
 
 		super.update(delta);
