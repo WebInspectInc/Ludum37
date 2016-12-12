6c37c782c9a6e751c1dc65634800ce7963d29ff9
David Dovel <davey5555@gmail.com>
2016-12-12 10:49:20 -0600

    randomize scorp speed

--- source/entities/enemies/Scorpion.hx
+++ source/entities/enemies/Scorpion.hx
@@ -28,7 +28,7 @@ class Scorpion extends Enemy {
 		animation.add('walk', [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25], 25);
 		animation.play('walk');
 
-		moveSpeed = 130;
+		moveSpeed = FlxG.random.float(100, 140);
 		setSize(52, 37);
 		offset.set(21, 33);
 
