9dd775db46451e4b1a499e7a3b08752a806dbd68
Timothy Miller <tjacobdesign@gmail.com>
2016-12-12 14:25:24 -0600

    Adds end music

--- source/LoseState.hx
+++ source/LoseState.hx
@@ -8,10 +8,13 @@ import flixel.ui.FlxButton;
 import flixel.math.FlxMath;
 import flixel.system.macros.FlxAssetPaths;
 import flixel.system.FlxAssets;
+import flixel.system.FlxSound;
 
 class LoseState extends FlxState
 {
 	public var image:FlxSprite;
+
+	private static var lossTheme:FlxSoundAsset = AssetPaths.lossTheme__wav;
 
 	public var loseImage:String = AssetPaths.you_died_screen__png;
 
@@ -22,6 +25,8 @@ class LoseState extends FlxState
 		image = new FlxSprite(0, 0, loseImage);
 		image.screenCenter();
 		add(image);
+
+		FlxG.sound.playMusic(lossTheme, 1, true);
 	}
 
 	override public function update(elapsed:Float):Void
